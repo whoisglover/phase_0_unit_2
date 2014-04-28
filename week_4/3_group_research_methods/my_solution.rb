6e183ada69e5229289fdc1d2a53b96db9f956181
whoisglover <dannyglover@outlook.com>
2014-04-28 00:30:58 -0500

    complete challenge 3

--- week_4/3_group_research_methods/my_solution.rb
+++ week_4/3_group_research_methods/my_solution.rb
@@ -76,7 +76,7 @@ end
 # .to_s was used on each value to account for digits.
 
 # Hash delete: I used almost the exact same logic as the array delete except changed the loop to .each_key to iterate through each of the keys of the hash. If the key
-# included (checked by if statement with .include?) the item I was looking to delete I called .delete on the source hash structure and used the current key as the input
+# included the item I was looking to delete (checked by if statement with .include?) I called .delete on the source hash structure and used the current key as the input.
 
 
 ################## DRIVER CODE ###################################
@@ -93,9 +93,30 @@ end
 p my_array_deletion_method(i_want_pets, "a") == ["I", 3, "pets", "but", "I", "only", 2 ]
 p my_hash_deletion_method(my_family_pets_ages, "George") == {"Evi" => 6, "Hoobie" => 3, "Bogart" => 4, "Poly" => 4, "Annabelle" => 0, "Ditto" => 3}
 
+
 # Reflect!
-# 
-# 
-# 
+=begin
+# What parts of your strategy worked? What problems did you face?
+		My strategy from the start was to use an each loop and check each index or hash key to see if it matched what I wanted.  In the end this turned out
+		well and worked efficiently. 
+What questions did you have while coding? What resources did you find to help you answer them?
+		Ruby-docs.org has become my best friend while researching built in methods.  I knew Ruby had some sort of include method and I was able to find all the 
+		detail I needed there.  It also taught me how to use the .delete method.  Both of these were key in making my part work (person 4).
+What concepts are you having trouble with, or did you just figure something out? If so, what?
+		I just figured out the .delete method for the first time.  It seems very straight forward and also quite powerful.  I love how ruby takes care of resizing arrays.
+		I am used to dealing with C code where array sizes are fixed.
+Did you learn any new skills or tricks?
+		I learned the methods .include and .delete.  I think I will be using both of these quite often in the future.
+How confident are you with each of the Learning Competencies?
+		I am completely confident with the Learning Competencies.
+Which parts of the challenge did you enjoy?
+		I enjoyed the challenge as a whole. It didn't seem to difficult and the answer came to me quickly.
+Which parts of the challenge did you find tedious?
+		No parts were tedious.
+# What did you learn about researching and explaining your research to others?
+		I learned that I can explain things to myself in my head much easier than trying to write them for others.  I need some more practice explaining what I'm thinking
+		and what the code is doing.  I also learned that it helps to break things into manageable steps to explain.  Explain one small part then move on the next one.  I hope 
+		my code makes sense to my team and I guess I will find out thru reviews.
+=end
 # 
 # 
