78a3d979a37f83acfeec04a06baecd74e420ce65
whoisglover <dannyglover@outlook.com>
2014-05-05 13:01:42 -0500

    boggle board reflection

--- week_5/4_boggle_board/my_solution.rb
+++ week_5/4_boggle_board/my_solution.rb
@@ -108,3 +108,19 @@ end
 Boggle.new(1).get_column ==  ["r", "o", "c", "a"] 
 
 # Reflection 
+
+What parts of your strategy worked? What problems did you face?
+    - Amol and I were a little confused as to how to pass the boggle board in to the class/where the boggle board should live
+    eventually we just decided to hard code it into the initialize
+What questions did you have while coding? What resources did you find to help you answer them?
+    - We didn't have many questions while working on this challenge, when we weren't sure exactly what was happening we used irb or printing to get a more clear picture
+What concepts are you having trouble with, or did you just figure something out? If so, what?
+    -Again we didn't have any trouble making this program do exactly what we wanted it to.  We didn't figure anything new out but solidified our understanding of classes
+Did you learn any new skills or tricks?
+    -No we didnt learn any new skills or tricks 
+How confident are you with each of the Learning Competencies?
+    -Amol and I both felt very confident after finishing this challenge.  We worked well together and were able to quickly and efficiently complete this challenge
+Which parts of the challenge did you enjoy?
+    - I enjoyed the boggle board being like a 2 dimensional matrix it refreshed my memory from earlier algorithm classes
+Which parts of the challenge did you find tedious?
+    - I didn't find any part of this challenge tedious 
